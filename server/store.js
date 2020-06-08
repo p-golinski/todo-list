7dd3e9036a42b16672e747f26bbd284b04f5a6c3
p-golinski <63773079+p-golinski@users.noreply.github.com>
2020-06-08 14:53:22 +0200

    Update store.js

--- server/store.js
+++ server/store.js
@@ -9,7 +9,21 @@ const init = async () => (
     })
   })
 )
+const listTasks = async () => (
+  new Promise((resolve, reject) => {
+    const query = new storage.TableQuery()
+      .select(['title'])
+      .where('PartitionKey eq ?', 'task')
+
+    service.queryEntities(table, query, null, (error, result) => {
+      !error ? resolve(result.entries.map((entry) => ({
+        title: entry.title._
+      }))) : reject()
+    })
+  })
+)
 
 module.exports = {
-  init
+  init,
+  listTasks
 }
