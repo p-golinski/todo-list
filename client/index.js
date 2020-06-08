a530eb8cec431943251798fdc0c6e738a326120e
p-golinski <63773079+p-golinski@users.noreply.github.com>
2020-06-08 16:02:03 +0200

    Update index.js

--- client/index.js
+++ client/index.js
@@ -2,8 +2,6 @@ const addTaskForm = document.querySelector('#addTaskForm')
 const addTaskTitle = document.querySelector('#addTaskForm #title')
 const addTaskBtn = document.querySelector('#addTaskBtn')
 const addTaskMsg = document.querySelector('#addTaskMsg')
-const tasksList = document.querySelector('#tasksList')
-const tasksListMsg = document.querySelector('#tasksListMsg')
 
 const addTask = async () => {
   const data = new FormData(addTaskForm)
@@ -27,93 +25,24 @@ addTaskForm.addEventListener('submit', (event) => {
   addTaskMsg.classList.remove('is-danger', 'is-success')
   addTaskMsg.classList.add('is-hidden')
 
-setTimeout(() => {
-  addTask()
-    .then((response) => {
-      if (!response.ok) {
-        throw Error(response.statusText)
-      }
-
-      addTaskMsg.textContent = 'Pomyślnie dodano zadanie.'
-      addTaskMsg.classList.add('is-success')
-      addTaskTitle.value = ''
-
-      listTasks()
-    })
-    .catch(() => {
-      addTaskMsg.textContent = 'Wystąpił błąd podczas dodawania zadania. Spróbuj ponownie później.'
-      addTaskMsg.classList.add('is-danger')
-    })
-    .finally(() => {
-      addTaskBtn.classList.remove('is-loading', 'is-disabled')
-      addTaskMsg.classList.remove('is-hidden')
-    })
-}, 1000)   
-})
-
-const listTasks = async () => {
-  tasksList.innerHTML = ''
-  tasksListMsg.classList.remove('is-danger')
-  tasksListMsg.classList.add('is-hidden')
-
-  fetch('/api/tasks')
-    .then((response) => {
-      if (!response.ok) {
-        throw Error(response.statusText)
-      }
-
-      return response.json()
-    })
-    .then((response) => {
-      response.forEach((task) => {
-        const title = document.createElement('td')
-        title.innerHTML = `<p>${task.title}</p>`
-
-        const actions = document.createElement('td')
-        actions.classList.add('has-text-right')
-        actions.innerHTML = `<button class="button is-small is-primary" id="deleteTask${task.id}" onclick="completeTask('${task.id}');"><span class="icon is-small"><i class="fas fa-check"></i></span></button>`
-
-        const row = document.createElement('tr')
-        row.appendChild(title)
-        row.appendChild(actions)
-
-        tasksList.appendChild(row)
-      })
-    })
-    .catch(() => {
-      tasksListMsg.textContent = 'Wystąpił błąd podczas pobierania listy zadań. Spróbuj ponownie później.'
-      tasksListMsg.classList.add('is-danger')
-    })
-}
-
-listTasks()
-
-const completeTask = (id) => {
-  tasksListMsg.classList.remove('is-danger')
-  tasksListMsg.classList.add('is-hidden')
-
-  const button = document.querySelector(`#deleteTask${id}`)
-  button.classList.add('is-loading')
-
   setTimeout(() => {
-    fetch(`/api/tasks?id=${id}`, { method: 'DELETE' })
+    addTask()
       .then((response) => {
         if (!response.ok) {
-          throw Error(response.statusText)
+          throw Error('Wystąpił błąd podczas dodawania zadania. Spróbuj ponownie później.')
         }
 
-        tasksListMsg.textContent = 'Pomyślnie usunięto zadanie.'
-        tasksListMsg.classList.add('is-success')
-
-        listTasks()
+        addTaskMsg.textContent = 'Pomyślnie dodano zadanie.'
+        addTaskMsg.classList.add('is-success')
+        addTaskTitle.value = ''
       })
-      .catch(() => {
-        button.classList.remove('is-loading')
-        tasksListMsg.textContent = 'Wystąpił błąd podczas usuwania zadania. Spróbuj ponownie później.'
-        tasksListMsg.classList.add('is-danger')
+      .catch((error) => {
+        addTaskMsg.textContent = error.message
+        addTaskMsg.classList.add('is-danger')
       })
       .finally(() => {
-        tasksListMsg.classList.remove('is-hidden')
+        addTaskBtn.classList.remove('is-loading', 'is-disabled')
+        addTaskMsg.classList.remove('is-hidden')
       })
-  }, 1000)
-}
+  }, 1000)    
+})
