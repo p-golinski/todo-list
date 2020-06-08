a7519c5ce3400e978bf322b9ba72eea454b087e4
p-golinski <63773079+p-golinski@users.noreply.github.com>
2020-06-08 15:45:55 +0200

    Update tasks.js

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -3,17 +3,16 @@ const store = require('../store')
 const router = new Router({ prefix: '/api/tasks' })
 
 router.get('/', async (ctx) => {
-  ctx.response.body = await store.listTasks()
+  ctx.status = 501
 })
 
 router.post('/', async (ctx) => {
   await store.addTask(ctx.request.body)
   ctx.status = 200
 })
+
 router.delete('/', async (ctx) => {
-  await store.deleteTask(ctx.request.query)
-  ctx.status = 200
-})
+  ctx.status = 501
 })
 
 module.exports = router
