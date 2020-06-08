0c2d4c007574543575993e482f661280afa17dc9
p-golinski <63773079+p-golinski@users.noreply.github.com>
2020-06-08 15:00:08 +0200

    Update tasks.js

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,17 +1,26 @@
 const Router = require('@koa/router')
-
 const router = new Router({ prefix: '/api/tasks' })
+const store = require('../store')
 
 router.get('/', async (ctx) => {
   ctx.response.body = await store.listTasks()
 })
 
 router.post('/', async (ctx) => {
-  ctx.status = 501
+  var task = ctx.request.body
+  if(task.title != ''){
+    await store.addTask(ctx.request.body)
+    console.log('addtask - post')
+    ctx.status = 200
+  }
+  else{
+    ctx.status = 400
+  }
 })
 
 router.delete('/', async (ctx) => {
-  ctx.status = 501
+  await store.deleteTask(ctx.request.query)
+  ctx.status = 200
 })
 
 module.exports = router
