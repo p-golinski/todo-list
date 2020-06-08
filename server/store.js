6ba64da3e2c96cd92eb0af05cbf625083c95111b
p-golinski <63773079+p-golinski@users.noreply.github.com>
2020-06-08 16:37:02 +0200

    Update store.js

--- server/store.js
+++ server/store.js
@@ -1,7 +1,7 @@
 const storage = require('azure-storage')
+const uuid = require('uuid')
 const service = storage.createTableService()
 const table = 'tasks'
-const uuid = require('uuid')
 
 const init = async () => (
   new Promise((resolve, reject) => {
@@ -10,6 +10,7 @@ const init = async () => (
     })
   })
 )
+
 const addTask = async ({ title }) => (
   new Promise((resolve, reject) => {
     const gen = storage.TableUtilities.entityGenerator
@@ -24,8 +25,37 @@ const addTask = async ({ title }) => (
     })
   })
 )
+const listTasks = async () => (
+  new Promise((resolve, reject) => {
+    const query = new storage.TableQuery()
+      .select(['RowKey', 'title'])
+      .where('PartitionKey eq ?', 'task')
+
+    service.queryEntities(table, query, null, (error, result) => {
+      !error ? resolve(result.entries.map((entry) => ({
+        id: entry.RowKey._,
+        title: entry.title._
+      }))) : reject()
+    })
+  })
+)
+const deleteTask = async ({ id }) => (
+  new Promise((resolve, reject) => {
+    const gen = storage.TableUtilities.entityGenerator
+    const task = {
+      PartitionKey: gen.String('task'),
+      RowKey: gen.String(id)
+    }
+
+    service.deleteEntity(table, task, (error) => {
+      !error ? resolve() : reject()
+    })
+  })
+)
 
 module.exports = {
   init,
   addTask,
+  listTasks,
+  deleteTask,
 }
