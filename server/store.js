200ae5ff2ce5b70e5f1771436c905ff04d500e29
p-golinski <63773079+p-golinski@users.noreply.github.com>
2020-06-08 16:20:40 +0200

    Update store.js

--- server/store.js
+++ server/store.js
@@ -1,7 +1,6 @@
 const storage = require('azure-storage')
 const service = storage.createTableService()
 const table = 'tasks'
-const uuid = require('uuid')
 
 const init = async () => (
   new Promise((resolve, reject) => {
@@ -10,22 +9,7 @@ const init = async () => (
     })
   })
 )
-const addTask = async ({ title }) => (
-  new Promise((resolve, reject) => {
-    const gen = storage.TableUtilities.entityGenerator
-    const task = {
-      PartitionKey: gen.String('task'),
-      RowKey: gen.String(uuid.v4()),
-      title
-    }
-
-    service.insertEntity(table, task, (error) => {
-      !error ? resolve() : reject()
-    })
-  })
-)
 
 module.exports = {
-  init,
-  addTask,
+  init
 }
