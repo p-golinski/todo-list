23fa313405346e40027db791096ec186cc48137e
p-golinski <63773079+p-golinski@users.noreply.github.com>
2020-06-08 12:50:37 +0200

    Dodano integrację z Azure Storage

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const path = require('path')
 const Koa = require('koa')
 const serve = require('koa-static')
-
 const app = new Koa()
 const port = process.env.PORT || 3000
+require('./store').init()
+
 
 app.use(serve(path.resolve(__dirname, '..', 'client')))
 
@@ -17,4 +18,4 @@ app.listen(port)
 
 console.log('App is listening at http://127.0.0.1:3000')
 
-require('./store').init()
+
